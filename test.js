d74ac9df6bb5fbe36706a6a9fc9d9770a6aeb58b
yoshuawuyts <i@yoshuawuyts.com>
2014-07-06 14:48:29 +0200

    test: add .default() tests

--- test.js
+++ test.js
@@ -9,10 +9,16 @@ var wayfarer = require('./index.js');
  * Test
  */
  
-describe('Wayfarer', function () {
+describe('wayfarer()', function () {
+  it('should initialize with empty properties', function() {
+    var router = wayfarer();
+    router.defaultPath.should.be.empty;
+  });
+});
+
+describe('.path()', function () {
   it('should catch type errors', function () {
     var router = wayfarer();
- 
     router.path.bind(router, 123)
       .should.throw('Path should be a string');
     router.path.bind(router, '/hi', 123)
@@ -20,7 +26,22 @@ describe('Wayfarer', function () {
     router.path.bind(router, '/hi', function(){})
       .should.not.throw();
   });
- 
+});
+
+describe('.default()', function () {
+  it('should catch errors', function() {
+    var router = wayfarer();
+    router.default.bind(router, 123)
+      .should.throw('Path should be a string');
+  });
+  it('should set a default path', function() {
+    var router = wayfarer();
+    router.default('404');
+    router.defaultPath.should.eql('404');
+  });
+});
+
+describe('.match()', function () {
   it('should match simple routes', function (done) {
     var router = wayfarer();
     router
@@ -29,7 +50,6 @@ describe('Wayfarer', function () {
 
     router.match('/hello');
   });
- 
   it('should match dynamic routes', function (done) {
     var router = wayfarer();
     router
@@ -38,4 +58,15 @@ describe('Wayfarer', function () {
 
     router.match('/tobi');
   });
+  it('should match the default path if no other paths match', function (done) {
+    var router = wayfarer();
+    router
+      .path('/hello', function() {})
+      .path('/howdy', function() {})
+      .path('/404', function() {done()})
+      .default('/404');
+
+    router.match('/anotherPath');
+  });
 });
+
