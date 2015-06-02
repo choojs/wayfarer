a8f8daabc4ce84f76fc9ba5bb637a43d448b34c2
Yoshua Wuyts <i@yoshuawuyts.com>
2015-06-02 02:29:41 +0200

    subrouting: init
    Closes #12. Probably slightly messy, but it works so hey. Also added back
    `const` since it works in older versions of node. When going for <=IE9 compat
    people should use a transpiler such as Babel.
    
    subrouting: use fix params

--- test.js
+++ test.js
@@ -36,15 +36,43 @@ test('.emit() should match partials', function (t) {
   r('/tobi')
 })
 
-test('.emit() should allow nesting', function (t) {
+test('.emit() should throw if no matches are found', function (t) {
   t.plan(1)
+  const r1 = wayfarer()
+  t.throws(r1.bind(r1, '/woops'), /path/)
+})
+
+test('.emit() should allow nesting', function (t) {
+  t.plan(5)
+
   const r1 = wayfarer()
   const r2 = wayfarer()
   r1.on('/home', r2)
-  r2.on('/home', function (uri) {
-    t.equal(uri, '/home')
+  r2.on('/', function (uri) {
+    t.equal(uri, '/')
   })
+
   r1('/home')
+
+  const r3 = wayfarer()
+  const r4 = wayfarer()
+  r3.on('/parent', r4)
+  r4.on('/child', function (uri) {
+    t.equal(uri, '/child')
+  })
+
+  r3('/parent/child')
+
+  const r5 = wayfarer()
+  const r6 = wayfarer()
+  r5.on('/:parent', r6)
+  r6.on('/child', function (uri, param) {
+    t.equal(typeof param, 'object')
+    t.equal(param.parent, 'hello')
+    t.equal(uri, '/child')
+  })
+
+  r5('/hello/child')
 })
 
 test('aliases', function (t) {
@@ -56,11 +84,14 @@ test('aliases', function (t) {
 test('.match()', function (t) {
   t.plan(3)
   const r = wayfarer()
+
   r.on('/user/:id', function (uri) {
     t.equal(uri, '/user/once', 'route not called with .match()')
   })
+
+  r('/user/once')
+
   const matched = r.match('/user/matched')
   t.equal(matched.param.id, 'matched')
   t.equal(typeof matched.node.cb, 'function')
-  r('/user/once')
 })
