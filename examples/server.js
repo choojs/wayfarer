d693673d19577cedd9cbd0ef656eb1a8ee1dfa77
Yoshua Wuyts <i@yoshuawuyts.com>
2015-07-30 02:51:41 +1000

    examples: server-methods -> server

--- examples/server.js
+++ examples/server.js
@@ -2,8 +2,8 @@ const methodist = require('methodist')
 const wayfarer = require('wayfarer')
 const http = require('http')
 
-const router = wayfarer()
 const server = http.createServer((req, res) => {
+  const router = wayfarer()
   router.on('/hello', methodist(req, {
     all: params => console.log('any route matches'),
     get: params => console.log('get')
