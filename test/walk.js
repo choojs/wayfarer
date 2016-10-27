c5e69cc2dc355772917969b02342f24d3104373b
William Castandet <wcastand@gmail.com>
2016-10-27 10:06:06 +0200

    fix test for walk.js by deleting es6 syntax

--- test/walk.js
+++ test/walk.js
@@ -4,20 +4,20 @@ const noop = require('noop2')
 const tape = require('tape')
 
 tape('walk', function (t) {
-  t.test('should assert input types', (t) => {
+  t.test('should assert input types', function (t) {
     t.plan(3)
     t.throws(walk.bind(null), /function/, 'assert first arg is function')
     t.throws(walk.bind(null, noop), /function/, 'assert second arg is a function')
     t.throws(walk.bind(null, noop, noop), /object/, 'assert trie exists')
   })
 
-  t.test('should walk a trie', (t) => {
+  t.test('should walk a trie', function (t) {
     t.plan(2)
     const router = wayfarer()
-    router.on('/foo', (x, y) => x * y)
-    router.on('/bar', (x, y) => x / y)
+    router.on('/foo', function (x, y) { return x * y })
+    router.on('/bar', function (x, y) { return x / y })
 
-    walk(router, (route, cb) => {
+    walk(router, function (route, cb) {
       const y = 2
       return function (params, x) {
         return cb(x, y)
@@ -28,14 +28,14 @@ tape('walk', function (t) {
     t.equal(router('/bar', 8), 4, 'divide')
   })
 
-  t.test('should walk a nested trie', (t) => {
+  t.test('should walk a nested trie', function (t) {
     t.plan(3)
     const router = wayfarer()
-    router.on('/foo/baz', (x, y) => x * y)
-    router.on('/bar/bin/barb', (x, y) => x / y)
-    router.on('/bar/bin/bla', (x, y) => x / y)
+    router.on('/foo/baz', function (x, y) { return x * y })
+    router.on('/bar/bin/barb', function (x, y) { return x / y })
+    router.on('/bar/bin/bla', function (x, y) { return x / y })
 
-    walk(router, (route, cb) => {
+    walk(router, function (route, cb) {
       const y = 2
       return function (params, x) {
         return cb(x, y)
@@ -47,15 +47,15 @@ tape('walk', function (t) {
     t.equal(router('/bar/bin/bla', 8), 4, 'divide')
   })
 
-  t.test('should walk partials', (t) => {
+  t.test('should walk partials', function (t) {
     t.plan(4)
     const router = wayfarer()
-    router.on('/foo', (route) => route)
-    router.on('/:foo', (route) => route)
-    router.on('/:foo/bar', (route) => route)
-    router.on('/:foo/:bar', (route) => route)
+    router.on('/foo', function (route) { return route })
+    router.on('/:foo', function (route) { return route })
+    router.on('/:foo/bar', function (route) { return route })
+    router.on('/:foo/:bar', function (route) { return route })
 
-    walk(router, (route, cb) => () => cb(route))
+    walk(router, function (route, cb) { return function () { return cb(route) } })
 
     t.equal(router('/foo'), '/foo', 'no partials')
     t.equal(router('/bleep'), '/:foo', 'one partial')
