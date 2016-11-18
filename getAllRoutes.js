62639f2f5234723e303b694142eefa6e41cbe4e4
William <wcastand@gmail.com>
2016-11-18 22:40:11 +0100

    feat(getAllRoutes): create get-all-routes function (#47)
    * create file getAllRoutes and add tests
    
    * pass array to object and delete es6, i think.
    
    * pass test in es5
    
    * forget some return statement
    
    * correct default parameter still es6 features
    
    * delete template strings
    
    * fix error in string sorry about that
    
    * delete Object.assign didn't know it was es6
    
    *  fix standard issue
    
    * update packages dependencies
    
    * Update package.json to test coverage
    
    * fix test for walk.js by deleting es6 syntax

---
+++ getAllRoutes.js
@@ -0,0 +1,35 @@
+const assert = require('assert')
+
+module.exports = getAllRoutes
+
+const transform = function (trie, previous) {
+  const prev = previous || '/'
+  const routes = {}
+  const nodes = trie.nodes
+  Object.keys(nodes).forEach(function (key) {
+    const path = (prev === '/' ? prev : prev + '/') + (key === '$$' ? ':' + trie.name : key)
+    const cb = nodes[key].cb
+    if (cb !== undefined) {
+      routes[path] = cb
+    }
+    if (Object.keys(nodes[key].nodes).length !== 0) {
+      const obj = transform(nodes[key], path)
+      Object.keys(obj).forEach(function (r) {
+        routes[r] = obj[r]
+      })
+    }
+  })
+  return routes
+}
+
+// walk a wayfarer trie
+// (obj, fn) -> null
+function getAllRoutes (router) {
+  assert.equal(typeof router, 'function', 'wayfarer.getAllRoutes: router should be an function')
+
+  const trie = router._trie
+  assert.equal(typeof trie, 'object', 'wayfarer.getAllRoutes: trie should be an object')
+
+  const tree = trie.trie
+  return transform(tree)
+}
---
+++ test/getAllRoutes.js
@@ -0,0 +1,58 @@
+const wayfarer = require('../')
+const getAllRoutes = require('../getAllRoutes')
+const tape = require('tape')
+
+tape('getAllRoutes', function (t) {
+  t.test('should assert input types', function (t) {
+    t.plan(1)
+    t.throws(getAllRoutes.bind(null), /function/, 'assert first arg is function')
+  })
+
+  t.test('should getAllRoutes', function (t) {
+    t.plan(4)
+    const router = wayfarer()
+    router.on('/foo', function (x, y) { return x * y })
+    router.on('/bar', function (x, y) { return x / y })
+
+    const routes = getAllRoutes(router)
+
+    t.equal(routes instanceof Object, true)
+    t.equal(Object.keys(routes).length, 2)
+    t.equal(typeof routes['/foo'], 'function')
+    t.equal(typeof routes['/bar'], 'function')
+  })
+
+  t.test('should getAllRoutes from a nested tree', function (t) {
+    t.plan(6)
+    const router = wayfarer()
+    router.on('/foo', function (x, y) { return x * y + 2 })
+    router.on('/foo/baz', function (x, y) { return x * y })
+    router.on('/bar/bin/barb', function (x, y) { return x / y })
+    router.on('/bar/bin/bla', function (x, y) { return x / y })
+
+    const routes = getAllRoutes(router)
+
+    t.equal(routes instanceof Object, true)
+    t.equal(Object.keys(routes).length, 4)
+    t.equal(typeof routes['/foo'], 'function')
+    t.equal(typeof routes['/foo/baz'], 'function')
+    t.equal(typeof routes['/bar/bin/barb'], 'function')
+    t.equal(typeof routes['/bar/bin/bla'], 'function')
+  })
+
+  t.test('should getAllRoutes from a routes with params', function (t) {
+    t.plan(5)
+    const router = wayfarer()
+    router.on('/foo', function (x, y) { return x / y })
+    router.on('/foo/:slug', function (x, y) { return x * y + 2 })
+    router.on('/foo/:slug/:id', function (x, y) { return x * y })
+
+    const routes = getAllRoutes(router)
+
+    t.equal(routes instanceof Object, true)
+    t.equal(Object.keys(routes).length, 3)
+    t.equal(typeof routes['/foo'], 'function')
+    t.equal(typeof routes['/foo/:slug'], 'function')
+    t.equal(typeof routes['/foo/:slug/:id'], 'function')
+  })
+})
